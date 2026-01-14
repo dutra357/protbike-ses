f03f730ac779032b26cc20b46a05324bcb0301ef
David Alves Dutra <dadutra@hotmail.com>
2026-01-14 20:37:18 -0300

    Inserida classe de  envio. Ajustado record de saida.

--- src/main/java/br/com/protbike/input/SqsToSesHandler.java
+++ src/main/java/br/com/protbike/input/SqsToSesHandler.java
@@ -1,5 +1,6 @@
 package br.com.protbike.input;
 
+import br.com.protbike.service.SesMailer;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
@@ -13,8 +14,9 @@ public class SqsToSesHandler implements RequestHandler<SQSEvent, SQSBatchRespons
     private final ObjectMapper mapper;
     private final SesMailer mailer;
 
-    public SqsToSesHandler(ObjectMapper mapper) {
+    public SqsToSesHandler(ObjectMapper mapper, SesMailer mailer) {
         this.mapper = mapper;
+        this.mailer = mailer;
     }
 
     @Override
--- src/main/java/br/com/protbike/service/SesMailer.java
+++ src/main/java/br/com/protbike/service/SesMailer.java
@@ -1,4 +1,37 @@
 package br.com.protbike.service;
 
+import br.com.protbike.records.EmailJob;
+import jakarta.enterprise.context.ApplicationScoped;
+import software.amazon.awssdk.services.sesv2.SesV2Client;
+import software.amazon.awssdk.services.sesv2.model.*;
+
+@ApplicationScoped
 public class SesMailer {
+
+    private final SesV2Client sesMailer;
+
+    public SesMailer(SesV2Client ses) {
+        this.sesMailer = ses;
+    }
+
+    public void send(EmailJob job) {
+        // Simples (HTML + text). Para casos avançados (anexos etc), use Raw email.
+        EmailContent content = EmailContent.builder()
+                .simple(Message.builder()
+                        .subject(Content.builder().data(job.subject).charset("UTF-8").build())
+                        .body(Body.builder()
+                                .html(Content.builder().data(job.html).charset("UTF-8").build())
+                                .text(Content.builder().data(job.text).charset("UTF-8").build())
+                                .build())
+                        .build())
+                .build();
+
+        SendEmailRequest req = SendEmailRequest.builder()
+                .fromEmailAddress(job.from)
+                .destination(Destination.builder().toAddresses(job.to).build())
+                .content(content)
+                .build();
+
+        sesMailer.sendEmail(req);
+    }
 }
