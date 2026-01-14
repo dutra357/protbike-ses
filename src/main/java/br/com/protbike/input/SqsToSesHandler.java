1d84c92190b0d723907055d0c7226430cea5176a
David Alves Dutra <dadutra@hotmail.com>
2026-01-14 20:29:51 -0300

    Ajustando classes e dependencias aws. Criando service de  envio.

--- src/main/java/br/com/protbike/input/SqsToSesHandler.java
+++ src/main/java/br/com/protbike/input/SqsToSesHandler.java
@@ -1,7 +1,24 @@
 package br.com.protbike.input;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.inject.Singleton;
 
 @Singleton
-public class SqsToSesHandler {
+public class SqsToSesHandler implements RequestHandler<SQSEvent, SQSBatchResponse> {
+
+    private final ObjectMapper mapper;
+    private final SesMailer mailer;
+
+    public SqsToSesHandler(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public SQSBatchResponse handleRequest(SQSEvent sqsEvent, Context context) {
+        return null;
+    }
 }
